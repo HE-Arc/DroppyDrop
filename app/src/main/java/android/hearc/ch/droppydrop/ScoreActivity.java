1bb31c339cf94f1086c8d34cc0a118c1416778d0
kim.biloni <kim.biloni@he-arc.ch>
2018-10-12 19:00:05 +0200

    add base elements in activity_score

--- app/src/main/java/android/hearc/ch/droppydrop/ScoreActivity.java
+++ app/src/main/java/android/hearc/ch/droppydrop/ScoreActivity.java
@@ -9,5 +9,6 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        // cup icon from : http://icons.iconarchive.com/icons/thesquid.ink/free-flat-sample/128/cup-icon.png
     }
 }
