481be702513c6f812aa223130e9ffc23d319443a
kim.biloni <kim.biloni@he-arc.ch>
2018-10-12 14:26:44 +0200

    add onClickListener on HomeActivity buttons

--- app/src/main/java/android/hearc/ch/droppydrop/HomeActivity.java
+++ app/src/main/java/android/hearc/ch/droppydrop/HomeActivity.java
@@ -2,12 +2,49 @@ package android.hearc.ch.droppydrop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button playButton;
+    Button scoreButton;
+    Button optionsButton;
+    Button creditsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        playButton = (Button) findViewById(R.id.playButton);
+        scoreButton = (Button) findViewById(R.id.scoreButton);
+        optionsButton = (Button) findViewById(R.id.optionsButton);
+        creditsButton = (Button) findViewById(R.id.creditsButton);
+
+        playButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                // TODO show LevelActivity
+            }
+        });
+        scoreButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO show ScoreActivity
+            }
+        });
+        optionsButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // TODO show OptionsActivity
+            }
+        });
+        creditsButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO popup credits
+            }
+        });
     }
 }
