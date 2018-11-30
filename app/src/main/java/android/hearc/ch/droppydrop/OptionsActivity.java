2d593fd965e640f1bae89d4e22d6e0d8d2f051f5
kim.biloni <kim.biloni@he-arc.ch>
2018-11-30 11:41:39 +0100

    first version of setting using sharedpreferences for username

--- app/src/main/java/android/hearc/ch/droppydrop/OptionsActivity.java
+++ app/src/main/java/android/hearc/ch/droppydrop/OptionsActivity.java
@@ -1,7 +1,10 @@
 package android.hearc.ch.droppydrop;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class OptionsActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class OptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        // https://developer.android.com/training/data-storage/shared-preferences
+        SharedPreferences sharedPref = getPreferences(this.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        String default_username = getResources().getString(R.string.default_username);
+        String username = sharedPref.getString(getString(R.string.username), default_username);
+        EditText usernameEditText = findViewById(R.id.username_editText);
+        usernameEditText.setText(username);
     }
 }
