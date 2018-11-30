384f8ffea646d7882c1859ed1fce58af4197665a
kim.biloni <kim.biloni@he-arc.ch>
2018-11-30 11:59:47 +0100

    saving username ok

--- app/src/main/java/android/hearc/ch/droppydrop/OptionsActivity.java
+++ app/src/main/java/android/hearc/ch/droppydrop/OptionsActivity.java
@@ -3,6 +3,8 @@ package android.hearc.ch.droppydrop;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,13 +15,35 @@ public class OptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
+        loadAndDisplayPreferences();
+
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePreferences();
+            }
+        });
+    }
+
+    private void loadAndDisplayPreferences()
+    {
         // https://developer.android.com/training/data-storage/shared-preferences
         SharedPreferences sharedPref = getPreferences(this.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
 
         String default_username = getResources().getString(R.string.default_username);
         String username = sharedPref.getString(getString(R.string.username), default_username);
         EditText usernameEditText = findViewById(R.id.username_editText);
         usernameEditText.setText(username);
+    }
+
+    private void savePreferences()
+    {
+        SharedPreferences sharedPref = getPreferences(this.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        EditText usernameEditText = findViewById(R.id.username_editText);
+        String newUsername = usernameEditText.getText().toString();
+        editor.putString(getString(R.string.username), newUsername);
+        editor.commit();
     }
 }
